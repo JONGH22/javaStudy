66a4b525f4958b3c7f727c7354eca4e64409a2d0
JONGH22 <whdgml1031@naver.com>
2019-05-20 19:24:00 +0900

    피보나치수열
    간단하게

--- TDD/src/Test.java
+++ TDD/src/Test.java
@@ -2,7 +2,20 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a1 = 1;
+		int a2 = 1;
+		int a3;
+        
+        System.out.println(a1);
+        System.out.println(a2);
+        
+        for(int i=1;; i++){
+            a3=a1+a2;
+            System.out.println(a3);
+            a1=a2;
+            a2=a3;
+            
+        }        
 
 	}
 
